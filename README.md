50652437efd1c370cb0e5596f2069bfb0474a325
HongKhanh <khanh10101998@gmail.com>
2021-06-08 13:51:05 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# TrainingTaskerV5
---
+++ chapter_1/lessons.js
@@ -0,0 +1,189 @@
+[
+  {
+    "block": "Bài 1: btaskee la gi"
+  },
+  {
+    "id": "0",
+    "type": "LESSON",
+    "title": "I. Mô hình bTaskee là gì? ne",
+    "time": 4,
+    "link": "https://raw.githubusercontent.com/bTaskee/TrainingTaskerV4/master/chapter_1/lesson_0.json"
+  },
+  {
+    "id": "1",
+    "type": "LESSON",
+    "title": "II. Định hướng nghề nghiệp.",
+    "time": 3,
+    "link": "https://raw.githubusercontent.com/bTaskee/TrainingTaskerV4/master/chapter_1/lesson_1.json"
+  },
+  {
+    "id": "2",
+    "type": "LESSON",
+    "title": "III. Mô tả cách Nhận việc",
+    "time": 10,
+    "link": "https://raw.githubusercontent.com/bTaskee/TrainingTaskerV4/master/chapter_1/lesson_2.json"
+  },
+  {
+    "id": "3",
+    "type": "LESSON",
+    "title": "IV. Lợi ích khi trở thành CTV",
+    "time": 10,
+    "link": "https://raw.githubusercontent.com/bTaskee/TrainingTaskerV4/master/chapter_1/lesson_3.json"
+  },
+  {
+    "block": "Bài 2: THÁI ĐỘ - TÁC PHONG - ỨNG XỬ"
+  },
+  {
+    "id": "4",
+    "type": "LESSON",
+    "title": "I. Thái độ",
+    "time": 4,
+    "link": "https://raw.githubusercontent.com/bTaskee/TrainingTaskerV4/master/chapter_1/lesson_5.json"
+  },
+  {
+    "id": "5",
+    "type": "LESSON",
+    "title": "II. Tác phong",
+    "time": 10,
+    "numberOfQuestions": 5,
+    "link": "https://raw.githubusercontent.com/bTaskee/TrainingTaskerV4/master/chapter_1/quizz_0.json"
+  },
+  {
+    "id": "6",
+    "type": "LESSON",
+    "title": "III. Ứng xử",
+    "time": 4,
+    "link": "https://raw.githubusercontent.com/bTaskee/TrainingTaskerV4/master/chapter_1/lesson_6.json"
+  },
+  {
+    "block": "Bài 3: KỸ NĂNG"
+  },
+
+  {
+    "id": "7",
+    "type": "LESSON",
+    "title": "I. CÁC NGUYÊN TẮC DỌN DẸP",
+    "time": 4,
+    "link": "https://raw.githubusercontent.com/bTaskee/TrainingTaskerV4/master/chapter_1/lesson_6.json"
+  },
+  {
+    "id": "8",
+    "type": "LESSON",
+    "title": "II. BỘ CÔNG CỤ DỤNG CỤ DỌN DẸP CƠ BẢN BTASKEE",
+    "time": 4,
+    "link": "https://raw.githubusercontent.com/bTaskee/TrainingTaskerV4/master/chapter_1/lesson_6.json"
+  },
+  {
+    "id": "9",
+    "type": "LESSON",
+    "title": "III. DỌN DẸP BẾP",
+    "time": 4,
+    "link": "https://raw.githubusercontent.com/bTaskee/TrainingTaskerV4/master/chapter_1/lesson_6.json"
+  },
+  {
+    "id": "10",
+    "type": "LESSON",
+    "title": "IV. DỌN DẸP PHÒNG NGỦ",
+    "time": 4,
+    "link": "https://raw.githubusercontent.com/bTaskee/TrainingTaskerV4/master/chapter_1/lesson_6.json"
+  },
+  {
+    "id": "11",
+    "type": "LESSON",
+    "title": "V. DỌN DẸP PHÒNG KHÁCH",
+    "time": 4,
+    "link": "https://raw.githubusercontent.com/bTaskee/TrainingTaskerV4/master/chapter_1/lesson_6.json"
+  },
+  {
+    "id": "12",
+    "type": "LESSON",
+    "title": "V. DỌN DẸP PHÒNG KHÁCH",
+    "time": 4,
+    "link": "https://raw.githubusercontent.com/bTaskee/TrainingTaskerV4/master/chapter_1/lesson_6.json"
+  },
+
+
+  {
+    "id": "13",
+    "type": "QUIZZ",
+    "title": "VI. DỌN DẸP NHÀ VỆ SINH",
+    "time": 10,
+    "numberOfQuestions": 5,
+    "link": "https://raw.githubusercontent.com/bTaskee/TrainingTaskerV4/master/chapter_1/quizz_1.json"
+  },
+  {
+    "id": "7",
+    "type": "LESSON",
+    "title": "Cách sử dụng chất tẩy rửa",
+    "time": 4,
+    "link": "https://raw.githubusercontent.com/bTaskee/TrainingTaskerV4/master/chapter_1/lesson_7.json"
+  },
+  {
+    "id": "2",
+    "type": "QUIZZ",
+    "title": "Bài kiểm tra cách sử dụng hóa chất",
+    "time": 10,
+    "numberOfQuestions": 5,
+    "link": "https://raw.githubusercontent.com/bTaskee/TrainingTaskerV4/master/chapter_1/quizz_2.json"
+  },
+  {
+    "id": "8",
+    "type": "LESSON",
+    "title": "Dọn nhà bếp",
+    "time": 4,
+    "link": "https://raw.githubusercontent.com/bTaskee/TrainingTaskerV4/master/chapter_1/lesson_8.json"
+  },
+  {
+    "id": "3",
+    "type": "QUIZZ",
+    "title": "Bài kiểm tra dọn dẹp bếp",
+    "time": 10,
+    "numberOfQuestions": 5,
+    "link": "https://raw.githubusercontent.com/bTaskee/TrainingTaskerV4/master/chapter_1/quizz_3.json"
+  },
+  {
+    "id": "9",
+    "type": "LESSON",
+    "title": "Dọn phòng ngủ",
+    "time": 4,
+    "link": "https://raw.githubusercontent.com/bTaskee/TrainingTaskerV4/master/chapter_1/lesson_9.json"
+  },
+  {
+    "id": "4",
+    "type": "QUIZZ",
+    "title": "Bài kiểm tra dọn phòng ngủ",
+    "time": 10,
+    "numberOfQuestions": 5,
+    "link": "https://raw.githubusercontent.com/bTaskee/TrainingTaskerV4/master/chapter_1/quizz_4.json"
+  },
+  {
+    "id": "10",
+    "type": "LESSON",
+    "title": "Dọn phòng khách",
+    "time": 4,
+    "link": "https://raw.githubusercontent.com/bTaskee/TrainingTaskerV4/master/chapter_1/lesson_10.json"
+  },
+  {
+    "id": "5",
+    "type": "QUIZZ",
+    "title": "Bài kiểm tra dọn phòng khách",
+    "time": 10,
+    "numberOfQuestions": 5,
+    "link": "https://raw.githubusercontent.com/bTaskee/TrainingTaskerV4/master/chapter_1/quizz_5.json"
+  },
+  {
+    "id": "11",
+    "type": "LESSON",
+    "title": "Dọn nhà vệ sinh",
+    "time": 4,
+    "link": "https://raw.githubusercontent.com/bTaskee/TrainingTaskerV4/master/chapter_1/lesson_11.json"
+  },
+  {
+    "id": "6",
+    "type": "QUIZZ",
+    "title": "Bài kiểm tra dọn nhà vệ sinh",
+    "time": 10,
+    "numberOfQuestions": 5,
+    "link": "https://raw.githubusercontent.com/bTaskee/TrainingTaskerV4/master/chapter_1/quizz_6.json"
+  }
+]
